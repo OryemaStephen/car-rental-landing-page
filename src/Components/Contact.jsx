d82bb6a0f7d128bd036fe351e1556a6f85aed50c
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-07-11 15:10:47 +0300

    Added comments

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,3 +1,5 @@
+import { Image } from "react-bootstrap"
+
 const Contact = () => {
   return (
     <>
@@ -5,7 +7,50 @@ const Contact = () => {
           <h3>What Our Customers Say About Us</h3>
           <p className="w-100 m-auto">
               Here are some comments from our customers, be one of them.
-          </p>
+          </p>   
+          <div className="comments">
+            <div className="comment align-content-start ">
+                <div className="profile d-flex justify-content-start align-items-center pb-2">
+                    <Image className="avatar" src="src\assets\profile.svg" />
+                    <div>
+                        <span className="name">Clifford</span><br />
+                        <span className="verifier">CapeTown, South Africa</span>
+                    </div>
+                </div>
+                <q>
+                  I had an incredible experience renting from Rent Cras! 
+                  The process was seamless, the car was in pristine condition, 
+                  and the customer service was top-notch..
+                </q>
+            </div>
+            <div className="comment">
+                <div className="profile d-flex justify-content-start align-items-center pb-2">
+                    <Image className="avatar" src="src\assets\profile.svg" />
+                    <div>
+                        <span className="name">Daniel </span><br />
+                        <span className="verifier">Nairobi, Kenya</span>
+                    </div>
+                </div>
+                <q>
+                  I was really impressed with the level of service I received
+                  from this car rental company. The process was smooth and easy, and the car
+                  I rented was in excellent condition..
+                </q>
+            </div>
+            <div className="comment">
+                <div className="profile d-flex justify-content-start align-items-center pb-2">
+                    <Image className="avatar" src="src\assets\profile.svg" />
+                    <div>
+                        <span className="name">Peter</span><br />
+                        <span className="verifier">Kampala, Uganda</span>
+                    </div>
+                </div>
+                <q>
+                  Rent Cars truly stands out. From their convenient online booking system to their 
+                  prompt pickup and drop-off service, everything was convenient and stress-free.
+                </q>
+            </div>
+          </div> 
       </div>
     </>
   )
