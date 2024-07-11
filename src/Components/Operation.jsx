38afda045d3eeae95254deb7a8f726ab3d0a4580
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-07-11 11:56:43 +0300

    Updated the working flow of the app

--- src/Components/Operation.jsx
+++ src/Components/Operation.jsx
@@ -1,4 +1,4 @@
-import { Image } from "react-bootstrap"
+import {  Image } from "react-bootstrap"
 import { FaCalendar, FaSearch, FaSmile } from "react-icons/fa"
 
 const Operation = () => {
@@ -12,12 +12,12 @@ const Operation = () => {
                 you to book and confirm your vehicle of choice online.
             </p>
         </div>
-        <div>
-            <div>
+        <div className="container-fluid working">
+            <div className="working-detail" >
                 <div>
-                    <div className=" bg-dark-subtle rounded-3 "><FaSearch /></div>
+                    <div className=" bg-dark-subtle "><FaSearch /></div>
                     <div>
-                        <h6>Browse and Select</h6>
+                        <h5>Browse and Select</h5>
                         <p>
                             Choose from our wide range of premium cars,
                             select the pickup and return dates and locations 
@@ -27,9 +27,9 @@ const Operation = () => {
                 </div>
 
                 <div>
-                    <div className=" bg-dark-subtle rounded-3 "><FaCalendar /></div>
+                    <div className=" bg-dark-subtle "><FaCalendar /></div>
                     <div>
-                        <h6>Book and Confirm</h6>
+                        <h5>Book and Confirm</h5>
                         <p>
                             Book your desired car with just a few clicks and receive an 
                             instant confirmation via email or SMS.
@@ -38,9 +38,9 @@ const Operation = () => {
                 </div>
 
                 <div>
-                    <div className=" bg-dark-subtle rounded-3 "><FaSmile /></div>
+                    <div className=" bg-dark-subtle "><FaSmile /></div>
                     <div>
-                        <h6>Enjoy your ride</h6>
+                        <h5>Enjoy your ride</h5>
                         <p>
                             Pick up your car at the designated location and enjoy your pre,ium driving 
                             experience with our top-quality service.
@@ -49,8 +49,8 @@ const Operation = () => {
                 </div>
 
             </div>
-            <div className="bg-dark rounded-4">
-                <Image src="https://65e81151f52e248c552b-fe74cd567ea2f1228f846834bd67571e.ssl.cf1.rackcdn.com/ldm-images/2021-BMW-3-Series-hero.png" />
+            <div className="working-image">
+                <Image src="https://bmw.scene7.com/is/image/BMW/iX?wid=3000&hei=3000" className=" working-img " />
             </div>
         </div>
     </>
